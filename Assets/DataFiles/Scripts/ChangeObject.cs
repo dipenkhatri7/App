09e0c5eff7602685f4796c5717da90129754f913
dipenkhatri7 <khatridipen7@gmail.com>
2022-08-05 23:09:53 +0545

    Canvas and ImageTarget

---
+++ Assets/DataFiles/Scripts/ChangeObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void nextObject(){
+        
+    }
+    public void previousObject(){
+
+    }
+
+}
